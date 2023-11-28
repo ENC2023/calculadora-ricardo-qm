fefb3322850f94e5b8f64f8a74dacac920875cdf
Ricardo Rafael Quintero Meza, PhD <84739791+ricardo-qm@users.noreply.github.com>
2023-11-28 19:47:35 +0000

    Refactor calculator operations and add documentation

--- app/src/main/java/miPrincipal/Calculadora.java
+++ app/src/main/java/miPrincipal/Calculadora.java
@@ -1,20 +1,52 @@
 package miPrincipal;
 
+/**
+ * Esta clase representa una calculadora que realiza operaciones matemáticas básicas.
+ */
 public class Calculadora {
 
+    /**
+     * Realiza la operación de suma entre dos números.
+     * @param x El primer número.
+     * @param y El segundo número.
+     * @return El resultado de la suma.
+     */
     public double suma(double x, double y) {
         return x + y;
     }
     
+    /**
+     * Realiza la operación de resta entre dos números.
+     * @param x El primer número.
+     * @param y El segundo número.
+     * @return El resultado de la resta.
+     */
     public double resta(double x, double y) {
-        return x-y;
+        return x - y;
     }
 
+    /**
+     * Realiza la operación de multiplicación entre dos números.
+     * @param x El primer número.
+     * @param y El segundo número.
+     * @return El resultado de la multiplicación.
+     */
     public double multiplica(double x, double y) {
-        return 0;
+        return x * y;
     }
 
+    /**
+     * Realiza la operación de división entre dos números.
+     * @param x El dividendo.
+     * @param y El divisor.
+     * @return El resultado de la división.
+     * @throws ArithmeticException Si el divisor es cero.
+     */
     public double divide(double x, double y) {
-        return 0;
+        if (y != 0) {
+            return x / y;
+        } else {
+            throw new ArithmeticException("No se puede dividir entre cero");
+        }
     }
 }
